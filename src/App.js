a1742d47d6f5594579f478f40ab72f9fb14762a4
MarkoBeg <markobegdev@gmail.com>
2022-01-23 16:34:55 +0100

    slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data";
+import { useState } from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function App() {
+  const [slide, setSlide] = useState(0);
+  const slidesData = data.length;
+
+  const handlePrev = () => {
+    setSlide(slide === 0 ? slidesData - 1 : slide - 1);
+  };
+
+  const handleNext = () => {
+    setSlide(slide === slidesData - 1 ? 0 : slide + 1);
+  };
+
+  console.log(slide);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <button onClick={handlePrev} className="prev">
+          <ArrowBackIosIcon></ArrowBackIosIcon>
+        </button>
+        <button onClick={handleNext} className="next">
+          <ArrowForwardIosIcon></ArrowForwardIosIcon>
+        </button>
+        {data.map((item, index) => {
+          return (
+            <div
+              className={index === slide ? "slide-active" : "slide"}
+              key={item.id}
+            >
+              {index === slide && <h2 className="title">{item.title}</h2>}
+              {index === slide && <img src={item.img} className="image"></img>}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    title: "Awesome Slider created by React!!",
+    img: "https://images.unsplash.com/photo-1642425149556-b6f90e946859?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 2,
+    title: "Awesome Slider created by React!!",
+    img: "https://images.unsplash.com/photo-1592452738030-cf604328ddf7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 3,
+    title: "Awesome Slider created by React!!",
+    img: "https://images.unsplash.com/photo-1641578858934-4a57aa69abd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 4,
+    title: "Awesome Slider created by React!!",
+    img: "https://images.unsplash.com/photo-1539471528744-ecf6aea851d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+];
+
+export default data;
